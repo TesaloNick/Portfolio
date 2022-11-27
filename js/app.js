35b1e974d547652c9b5a4e9160915daff79f6452
Nikolay Antonovich <TesaloNick@gmail.com>
2022-11-27 21:14:55 +0300

    fix header, add about

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 const burger = document.querySelector('.home__burger')
 const burgerWrapper = document.querySelector('.home__burger_wrapper')
 const nav = document.querySelector('.home__nav')
-nav.style.top = (-nav.offsetHeight + 50) + 'px' // начальное положение меню
+const header = document.querySelector('.home__header')
 
 burger.addEventListener('click', () => {
   burgerWrapper.classList.toggle('open')
   nav.classList.toggle('open')
+})
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 50) {
+    console.log(1);
+    header.classList.add('white')
+  } else {
+    header.classList.remove('white')
+  }
 })
