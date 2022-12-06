95c9f982c3a02096eb6a5a9e1b64fd90f47df309
Nikolay Antonovich <TesaloNick@gmail.com>
2022-12-06 16:17:59 +0300

    tried netlify forms

--- js/app.js
+++ js/app.js
@@ -129,23 +129,22 @@ function slowScroll(id) {
 }
 
 //------------------------------send email-------------------------
-$(document).ready(function () {
-  //E-mail Ajax Send
-  $(".form form").submit(function () { //Change
-    console.log('1');
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function () {
-      alert("Thank you!");
-      setTimeout(function () {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
+// $(document).ready(function () {
+//   //E-mail Ajax Send
+//   $(".form form").submit(function () { //Change
+//     var th = $(this);
+//     $.ajax({
+//       type: "POST",
+//       url: "./mail.php", //Change
+//       data: th.serialize()
+//     }).done(function () {
+//       alert("Thank you!");
+//       setTimeout(function () {
+//         // Done Functions
+//         th.trigger("reset");
+//       }, 1000);
+//     });
+//     return false;
+//   });
 
-});
+// });
